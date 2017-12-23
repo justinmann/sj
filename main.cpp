6c54f24641557714d8ad15b5bee32e3d9827f38b
Justin Mann <justin.mann@gmail.com>
2017-12-23 11:01:08 -0800

    command line options

--- main.cpp
+++ main.cpp
@@ -2,113 +2,73 @@
 #include <string.h>
 #include <fstream>
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
 namespace fs = boost::filesystem;
-
-void runTest(fs::path path, bool updateResult) {    
-	if (path.extension().string().compare(".sj") == 0) {
-		printf("Running %s\n", path.string().c_str());
-        TrBlock::resetVarNames();
-
-        auto codeFileName = fs::change_extension(path, ".c");
-        auto debugFileName = fs::change_extension(path, ".debug");
-        auto errorFileName = fs::change_extension(path, ".errors");
-
-		if (updateResult) {
-			ofstream code;
-			code.open(codeFileName.c_str());
-			ofstream error;
-			error.open(errorFileName.c_str());
-            ofstream debug;
-            debug.open(debugFileName.c_str());
-
-			Compiler compiler;
-			compiler.transpile(path.string(), code, error, &debug);
-
-			code.close();
-			error.close();
-            debug.close();
-		} else {
-			stringstream codeA;
-			stringstream errorA;
-
-			Compiler compiler;
-			compiler.transpile(path.string(), codeA, errorA, nullptr);
-
-			codeA.seekg(0, codeA.beg);
-			errorA.seekg(0, errorA.beg);
-
-			// Compare file output
-			int line = 0;
-			string lineA;
-			string lineB;
-			ifstream codeB(codeFileName.c_str());
-			if (codeB.is_open())
-			{
-				while (getline(codeB, lineB))
-				{
-					line++;
-					getline(codeA, lineA);
-
-					if (lineA.compare(lineB) != 0) {
-						cout << "Line " << line << " does not match:\n" << lineA << "\n" << lineB << "\n";
-					}
-				}
-				codeB.close();
-			}
-
-			// Compare error output
-			line = 0;
-			ifstream errorB(errorFileName.c_str());
-			if (errorB.is_open())
-			{
-				while (getline(errorB, lineB))
-				{
-					line++;
-					getline(errorA, lineA);
-
-					if (lineA.compare(lineB) != 0) {
-						cout << "Error " << line << " does not match:\n" << lineA << "\n" << lineB << "\n";
-					}
-				}
-				errorB.close();
-			}
-		}
-	}
-}
-
-void runAllTests(fs::path path, bool updateResult, const char* wildcard) {
-    for (auto child : fs::directory_iterator(path)) {
-        if (fs::is_regular_file(child.path()) && (wildcard == nullptr || child.path().generic_string().find(wildcard) != string::npos)) {
-            runTest(child.path(), updateResult);
-        }
-    }
-}
-                          
+namespace po = boost::program_options;
+           
 int main(int argc, char **argv) {
-    shared_ptr<CResult> result;
-    Compiler compiler;
-    
-    if (argc == 1) {
-        printf("-test or [filename]\n");
-        return 0;
-    }
-    
-	if (strcmp(argv[1], "-test") == 0) {
-        runAllTests("app", false, argv[2]);
-		runAllTests("basic", false, argv[2]);
-	} else if (strcmp(argv[1], "-testUpdate") == 0) {
-        runAllTests("app", true, argv[2]);
-        runAllTests("basic", true, argv[2]);
-    } else {
-        auto path = fs::path(argv[1]);
-        auto codeFileName = fs::change_extension(path, ".c");
-        ofstream code;
-        code.open(codeFileName.c_str());
+    po::options_description generic_options("Generic options");
+    generic_options.add_options()
+        ("help", "show help")
+        ;
 
-        Compiler compiler;
-        compiler.transpile(path.string(), code, cerr, nullptr);
+    po::options_description config_options("Configuration");
+    config_options.add_options()
+        ("no-lines", "do not output #lineno directive")
+        ("wasm", "output wasm (not supported yet)")
+        ("app", "output app for current system (not supported yet)")
+        ("debug", "output debug files")
+        ;
+
+    po::options_description hidden_options("Hidden options");
+    hidden_options.add_options()
+        ("sj-file", "file to compile")
+        ;
+
+    po::options_description cmdline_options;
+    cmdline_options.add(generic_options).add(config_options).add(hidden_options);
+
+    po::options_description config_file_options;
+    config_file_options.add(config_options).add(hidden_options);
+
+    po::options_description visible("Allowed options");
+    visible.add(generic_options).add(config_options);
+
+    po::positional_options_description p;
+    p.add("sj-file", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+        options(cmdline_options).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help") || !vm.count("sj-file")) {
+        cout << visible << "\n";
+        return 1;
     }
+
+    bool outputLines = vm.count("no-lines");
+    bool outputWasm = vm.count("wasm");
+    bool outputApp = vm.count("app");
+    bool outputDebug = vm.count("debug");
+    string sjFilename = vm["sj-file"].as<string>();
+
+    auto path = fs::path(sjFilename);
+    auto codeFileName = fs::change_extension(path, ".c");
+    ofstream code;
+    code.open(codeFileName.c_str());
+
+    ofstream* debugStream = nullptr;
+    ofstream debug;
+    if (outputDebug) {
+        auto debugFileName = fs::change_extension(path, ".debug");
+        debug.open(debugFileName.c_str());
+        debugStream = &debug;
+    }
+
+    Compiler compiler(outputLines);
+    compiler.transpile(path.string(), code, cerr, debugStream);
 
     return 0;
 }
